fea5ba3b9e33faf0f94dfc5d879f16aceeea2806
Anton Belov <antonioshch14@gmail.com>
2020-01-20 08:30:12 +0300

    succesfully get data from server and passes to UNO

--- ESP_1.ino
+++ ESP_1.ino
@@ -47,7 +47,7 @@ void Send_Request_To_Server() {
 			+ "humid:" + getData[0][2] + ";" + "humidAv:" + getData[0][3] + ";" +
 			"status:" + getData[0][4] + ";" ;
 		TCP_Client.println(dataToSend);
-		Serial.print("- data stream: ");	Serial.println(dataToSend);//Send sensor data
+		//Serial.print("- data stream: ");	Serial.println(dataToSend);//Send sensor data
 
 
 	}
@@ -108,11 +108,11 @@ void Tell_Server_we_are_there() {
 //====================================================================================
 
 void loop() {
-	
+
 	//WiFiClient::setLocalPortStart(2391);
 	Check_WiFi_and_Connect_or_Reconnect();          // Checking For Connection
 	//Send_Request_To_Server();
-if (millis() - lastUpdateTime >= postingInterval)
+	if (millis() - lastUpdateTime >= postingInterval)
 	{
 		readDataSerial(row);
 		if (row > 0) { Send_Request_To_Server(); row = 0; memset(getData, 0, sizeof(getData)); }
@@ -124,19 +124,56 @@ if (millis() - lastUpdateTime >= postingInterval)
 
 		if (TCP_Client.available() > 0) {                     // Check For Reply
 			String line = TCP_Client.readStringUntil('\r');     // if '\r' is found
-			Serial.print("received: ");                         // print the content
+			//Serial.print("received: ");                         // print the content
 			Serial.println(line);
-			
+
 		}
 	}
 	else Tell_Server_we_are_there();
 	//	}
-}
+	
+	
+			/*unsigned long value;
+			int index;
+			int humid;
+			if (get_field_value(message, "humid:", &value, &index)) {
+				humid = value / pow(10, index);
+				Serial.print("Humid recognised: ");                         // print the content
+				Serial.println(humid);
+			}*/
+		
 
-  
-  //jsonBuffer[0] = '['; //Reinitialize the jsonBuffer for next batch of data
-  //jsonBuffer[1] = '\0';
- 
+
+}
+bool get_field_value(String Message, String field, unsigned long* value, int* index) {
+	int fieldBegin = Message.indexOf(field) + field.length();
+	int check_field = Message.indexOf(field);
+	int ii = 0;
+	*value = 0;
+	*index = 0;
+	bool indFloat = false;
+	if (check_field != -1) {
+		int filedEnd = Message.indexOf(';', fieldBegin);
+		if (filedEnd == -1) { return false; }
+		int i = 1;
+		char ch = Message[filedEnd - i];
+		while (ch != ' ' && ch != ':') {
+			if (isDigit(ch)) {
+				int val = ch - 48;
+				if (!indFloat)ii = i - 1;
+				else ii = i - 2;
+				*value = *value + ((val * pow(10, ii)));
+			}
+			else if (ch == '.') { *index = i - 1; indFloat = true; }
+			i++;
+			if (i > (filedEnd - fieldBegin + 1) || i > 10)break;
+			ch = Message[filedEnd - i];
+		}
+
+	}
+	else return false;
+	return true;
+}
 
 void readDataSerial(int& rowRead)
 { if (Serial.available()){
